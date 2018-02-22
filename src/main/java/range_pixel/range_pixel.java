9875ae285283dd7aa40bba0cbdeea593a92ba92b
21588204@qq.com <21588204@qq.com>
2018-02-22 14:28:27 -0700

    update

--- src/main/java/range_pixel/range_pixel.java
+++ src/main/java/range_pixel/range_pixel.java
@@ -8,9 +8,9 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class range_pixel {
-    String pixelPath = "pixel.csv";
+    String pixelPath = "data/pixel/pixel.csv";
     //    String GPSData = "data/pixel/sample_gps.csv";
-    String GPSData = "coordinate_points.csv";
+    String GPSData = "data/pixel/coordinate_points.csv";
 
 
     HashMap<Long, Pair<Double, Double>> pixelList = new HashMap<Long, Pair<Double, Double>>(); //pixel_id -> <northing,easting>
@@ -332,7 +332,7 @@ public class range_pixel {
             }
             br.close();
         } catch (Exception e) {
-//            e.printStackTrace();
+            e.printStackTrace();
             System.err.println("Can not open the Pixel file, please check it. ");
         }
         System.out.println("read the pixel file done" + "   " + linenumber);
